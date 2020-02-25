227e22b73d865dc1ca145fdb71a11df2177f9268
gabriel <gabrielcunhadev@gmail.com>
2020-02-25 13:23:47 -0300

    Initial Commit.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './dashboard/home/home.component';
+import { LivroComponent } from './livro/livro.component';
 
 
-const routes: Routes = [];
+export const ROUTES: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'livro', component: LivroComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(
+    ROUTES,
+    { enableTracing: true } // <-- debugging purposes only) 
+    )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './dashboard/header/header.component';
+import { FooterComponent } from './dashboard/footer/footer.component';
+import { HomeComponent } from './dashboard/home/home.component';
+import { MenuComponent } from './dashboard/menu/menu.component';
+import { SidebarComponent } from './dashboard/menu/sidebar/sidebar.component';
+import { LivroComponent } from './livro/livro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    MenuComponent,
+    SidebarComponent,
+    LivroComponent
   ],
   imports: [
     BrowserModule,
